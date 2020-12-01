651366a62a626642726e0f19016da98908941fdb
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-01 12:48:17 -0800

    combinations, yah

--- 2020/rust/day-01/src/main.rs
+++ 2020/rust/day-01/src/main.rs
@@ -7,7 +7,7 @@ fn process(input: &str) -> Option<u64> {
     input
         .lines()
         .map(|string_num| string_num.parse::<u64>().unwrap())
-        .permutations(3)
+        .combinations(3)
         .find(|perm| perm.iter().sum::<u64>() == 2020)
         .map(|v| v.iter().product())
 }
