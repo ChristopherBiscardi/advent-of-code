47c1d16883fdc1e0ff888a725f7c5b7aa95f8042
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-06 13:48:21 -0800

    post stream day 6

---
+++ 2020/rust/day-06/benches/benchmark.rs
@@ -0,0 +1,12 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_06::process_part2;
+
+const INPUT: &'static str = include_str!("../input.txt");
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("day-04:part-2", |b| {
+        b.iter(|| process_part2(black_box(INPUT)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
