d3ef73f4cdabebdb49ffb8b70c5c598697d5f86e
Christopher Biscardi <chris@christopherbiscardi.com>
2020-12-09 14:54:05 -0800

    done day 9

---
+++ 2020/rust/day-09/benches/benchmark.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_09::{process_part1, process_part2};
+
+const INPUT: &'static str = include_str!("../input.txt");
+fn bench_1(c: &mut Criterion) {
+    c.bench_function("day-09:part-1", |b| {
+        b.iter(|| process_part1(black_box(INPUT), 25))
+    });
+}
+
+fn bench_2(c: &mut Criterion) {
+    c.bench_function("day-09:part-2", |b| {
+        b.iter(|| process_part2(black_box(INPUT), 25))
+    });
+}
+
+criterion_group!(benches, bench_1, bench_2);
+criterion_main!(benches);
---
+++ 2020/rust/day-09/src/part2.rs
@@ -0,0 +1,22 @@
+use day_09::process_part2;
+use std::env;
+use std::fs;
+use std::io::{Error, ErrorKind};
+
+fn main() -> Result<(), std::io::Error> {
+    //     Right 1, down 1.
+    // Right 3, down 1. (This is the slope you already checked.)
+    // Right 5, down 1.
+    // Right 7, down 1.
+    // Right 1, down 2.
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err(Error::new(ErrorKind::Other, "An input file is required"));
+    }
+    let input = fs::read_to_string(&args[1])?;
+    let result = process_part2(&input, 25);
+    println!("{:?}", result);
+    Ok(())
+}
