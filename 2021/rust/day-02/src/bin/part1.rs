1e4de466e1b1a0b9c597d9297d93cc8c0c1a2e64
Christopher Biscardi <chris@christopherbiscardi.com>
2021-12-02 10:38:46 -0800

    solutions work

---
+++ 2021/rust/day-02/src/bin/part1.rs
@@ -0,0 +1,53 @@
+#[cfg(feature = "dhat")]
+use dhat::{Dhat, DhatAlloc};
+use std::fs;
+
+#[cfg(feature = "dhat")]
+#[global_allocator]
+static ALLOCATOR: DhatAlloc = DhatAlloc;
+
+#[derive(Default, Debug)]
+struct Submarine {
+    x: i32,
+    y: i32,
+}
+
+impl Submarine {
+    fn finalize(&self) -> i32 {
+        self.x * self.y
+    }
+}
+enum Direction {
+    Forward(i32),
+    Up(i32),
+    Down(i32),
+}
+fn main() {
+    #[cfg(feature = "dhat")]
+    let _dhat = Dhat::start_heap_profiling();
+
+    let file = fs::read_to_string("./input.txt").unwrap();
+    let final_position = file.lines().fold(
+        Submarine::default(),
+        |mut acc, item| {
+            let items =
+                item.split(' ').collect::<Vec<&str>>();
+            let magnitude =
+                items[1].parse::<i32>().unwrap();
+            match items[0] {
+                "forward" => {
+                    acc.x += magnitude;
+                }
+                "up" => {
+                    acc.y -= magnitude;
+                }
+                "down" => {
+                    acc.y += magnitude;
+                }
+                i => panic!("unhandled input {}", i),
+            };
+            acc
+        },
+    );
+    dbg!(final_position.finalize());
+}
---
+++ 2021/rust/day-02/src/lib.rs
@@ -0,0 +1,1 @@
+pub fn part1() {}
