3e605e9f88b2984cd079216a50dea868a4b5a07c
Christopher Biscardi <chris@christopherbiscardi.com>
2021-12-02 10:02:35 -0800

    dhat as a feature

--- 2021/rust/day-01/src/bin/part2.rs
+++ 2021/rust/day-01/src/bin/part2.rs
@@ -1,13 +1,13 @@
-#[cfg(dhat)]
+#[cfg(feature = "dhat")]
 use dhat::{Dhat, DhatAlloc};
 use std::fs;
 
-#[cfg(dhat)]
+#[cfg(feature = "dhat")]
 #[global_allocator]
 static ALLOCATOR: DhatAlloc = DhatAlloc;
 
 fn main() {
-    #[cfg(dhat)]
+    #[cfg(feature = "dhat")]
     let _dhat = Dhat::start_heap_profiling();
 
     let file = fs::read_to_string("./input.txt").unwrap();
