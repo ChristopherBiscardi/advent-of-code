63fe303896da6d4222be21a826967517058fee86
Christopher Biscardi <chris@christopherbiscardi.com>
2021-12-10 22:56:35 -0800

    part 2 test input works

--- 2021/rust/day-10/src/lib.rs
+++ 2021/rust/day-10/src/lib.rs
@@ -1,14 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 
 use nom::{
-    branch::alt,
-    bytes::streaming::take_until,
-    character::streaming::{
-        alpha1, anychar, char, digit1, newline, one_of, u16,
+    character::{
+        complete::{self},
+        streaming::{char, one_of},
     },
-    combinator::opt,
     error::context,
-    multi::{many0, many1, separated_list1},
+    multi::{many0, many1},
     IResult,
 };
 use nom_supreme::error::StackContext::Context;
@@ -113,15 +111,293 @@ pub fn process_part1(input: &str) -> u32 {
     results
 }
 
-fn puzzle_input_2(input: &str) -> IResult<&str, ()> {
-    // let (input, outputs) =
-    //     separated_list1(newline, row_2)(input)?;
-    todo!("part 2")
+// fn chunk_2(
+//     original_input: &str,
+// ) -> IResult<&str, String, ErrorTree<&str>> {
+//     dbg!("chunk2");
+//     if original_input == "" {
+//         Ok(("", "".to_string()))
+//     } else {
+//         let (input, open_char) =
+//             one_of("({<[")(original_input)?;
+//         let c_res: IResult<&str, &str, ErrorTree<&str>> =
+//             tag(match open_char {
+//                 '{' => "}",
+//                 '(' => ")",
+//                 '[' => "]",
+//                 '<' => ">",
+//                 _ => panic!("unrecognized char"),
+//             })(input);
+//         match c_res {
+//             Ok((input, something)) => {
+//                 dbg!(&input, &something);
+//                 Ok((
+//                     input,
+//                     open_char.to_string() + something,
+//                 ))
+//             }
+//             Err(nom::Err::Incomplete(_)) => {
+//                 dbg!("incomplete here");
+//                 Ok((
+//                     input,
+//                     match open_char {
+//                         '{' => "}",
+//                         '(' => ")",
+//                         '[' => "]",
+//                         '<' => ">",
+//                         _ => panic!("unrecognized char"),
+//                     }
+//                     .to_string(),
+//                 ))
+//             }
+//             Err(e) => {
+//                 // dbg!(&e);
+//                 dbg!(&input);
+//                 let (input, leftovers) =
+//                     many1(chunk_2)(input)?;
+//                 dbg!(&leftovers);
+//                 Ok((
+//                     input,
+//                     leftovers
+//                         .into_iter()
+//                         .collect::<String>(),
+//                 ))
+//             }
+//         }
+//     }
+
+// if let Ok((input, _)) = c_res {
+//     Ok((input, "".to_string()))
+// } else {
+//     let (input, leftovers) =
+//         context("chunk", many0(chunk))(input)?;
+
+//     let chars_res = context(
+//         "chars",
+//         tag(match open_char {
+//             '{' => "}",
+//             '(' => ")",
+//             '[' => "]",
+//             '<' => ">",
+//             _ => panic!("unrecognized char"),
+//         }),
+//     )(input);
+//     dbg!(&chars_res);
+//     match chars_res {
+//         Ok((input, c)) => {
+//             dbg!(&input, &c);
+//             Ok((input, "".to_string()))
+//         }
+//         Err(e) => {
+//             dbg!(&e);
+//             match e {
+//                 nom::Err::Incomplete(_) => Ok((
+//                     input,
+//                     match open_char {
+//                         '{' => "}",
+//                         '(' => ")",
+//                         '[' => "]",
+//                         '<' => ">",
+//                         _ => panic!("here"),
+//                     }
+//                     .to_string(),
+//                 )),
+//                 e => Err(e),
+//             }
+//         }
+//     }
+//     // Ok((input, "".to_string()))
+// }
+// }
+
+fn chunk_2(original_input: &str) -> IResult<&str, Ast> {
+    // println!("chunk_2: {}", &original_input);
+    let (input, open_char) =
+        complete::one_of("({<[")(original_input)?;
+    let c_res: IResult<&str, char> =
+        complete::char(match open_char {
+            '{' => '}',
+            '(' => ')',
+            '[' => ']',
+            '<' => '>',
+            _ => panic!("unrecognized char"),
+        })(input);
+    match c_res {
+        Ok((input, close_char)) => {
+            // dbg!(&input, &close_char);
+            Ok((
+                input,
+                Ast {
+                    left: open_char,
+                    right: Some(close_char),
+                    children: vec![],
+                },
+            ))
+        }
+        // Err(nom::Err::Incomplete(_)) => {
+        //     // println!("incomplete: {}", &input);
+        //     Ok((
+        //         input,
+        //         Ast {
+        //             left: open_char,
+        //             right: None,
+        //             children: vec![],
+        //         },
+        //     ))
+        // }
+        Err(e) => {
+            if input == "" {
+                Ok((
+                    input,
+                    Ast {
+                        left: open_char,
+                        right: None,
+                        children: vec![],
+                    },
+                ))
+            } else {
+                // dbg!(input);
+                let (input, output) =
+                    many1(chunk_2)(input)?;
+                //                 println!(
+                //                     "i: {}
+                // o: {:?}",
+                //                     &e, &output
+                //                 );
+                let c_res: IResult<&str, char> =
+                    char(match open_char {
+                        '{' => '}',
+                        '(' => ')',
+                        '[' => ']',
+                        '<' => '>',
+                        _ => panic!("unrecognized char"),
+                    })(input);
+                match c_res {
+                    Ok((input, c)) => Ok((
+                        input,
+                        Ast {
+                            left: open_char,
+                            right: Some(c),
+                            children: output,
+                        },
+                    )),
+                    Err(nom::Err::Incomplete(_)) => Ok((
+                        input,
+                        Ast {
+                            left: open_char,
+                            right: None,
+                            children: output,
+                        },
+                    )),
+                    Err(e) => Err(e),
+                }
+            }
+        }
+    }
+}
+#[derive(Debug, PartialEq)]
+struct Ast {
+    left: char,
+    right: Option<char>,
+    children: Vec<Ast>,
 }
 
-pub fn process_part2(input: &str) -> usize {
-    let (_, heightmap) = puzzle_input_2(input).unwrap();
-    todo!("part2")
+impl fmt::Display for Ast {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(
+            f,
+            "{}{}",
+            // self.left,
+            self.children
+                .iter()
+                .map(|v| v.to_string())
+                .collect::<String>(),
+            if let None = self.right {
+                match self.left {
+                    '{' => "}",
+                    '(' => ")",
+                    '[' => "]",
+                    '<' => ">",
+                    _ => panic!("unrecognized char"),
+                }
+            } else {
+                // self.right.unwrap()
+                ""
+            }
+        )
+    }
+}
+
+pub fn process_part2(input: &str) -> u32 {
+    let mut results: Vec<u32> = input
+        .lines()
+        .map(|line| {
+            let res = chunk_2(line);
+            match res {
+                Ok((input, output)) => {
+                    if input.len() > 0 {
+                        chunk_2(input)
+                    } else {
+                        Ok((input, output))
+                    }
+                }
+                Err(e) => Err(e),
+            }
+        })
+        .enumerate()
+        // filter out lines that end early
+        .filter_map(|(i, res)| {
+            // dbg!(&res);
+            match res {
+                Ok((input, v)) => {
+                    let num = v.to_string().chars().fold(
+                        0,
+                        |acc, v| {
+                            acc * 5
+                                + match v {
+                                    ')' => 1,
+                                    ']' => 2,
+                                    '}' => 3,
+                                    '>' => 4,
+                                    _ => panic!("askflj"),
+                                }
+                        },
+                    );
+                    Some(num)
+                }
+                Err(e) => {
+                    // dbg!(e);
+                    None
+                }
+
+                // Err(ErrorTree::Stack {
+                //     base,
+                //     contexts,
+                // }) => {
+                //     dbg!(base);
+                //     // dbg!(&base, &contexts);
+                //     // let ctx = contexts
+                //     //     .iter()
+                //     //     .find(|v| v.1 == Context("chars"))
+                //     //     .unwrap();
+                //     // // dbg!(ctx);
+                //     // let c = ctx.0.chars().next().unwrap();
+                //     // let res = scoring.get(&c);
+                //     // // dbg!(c);
+                //     // res
+                //     Some(0)
+                // }
+                _ => panic!("uh oh"),
+            }
+        })
+        .collect();
+    results.sort();
+    results[2]
 }
 
 #[cfg(test)]
@@ -255,8 +531,51 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn part2_test_demo_data() {
-    //     assert_eq!(1134, process_part2(input));
-    // }
+    #[test]
+    fn part2_test_demo_data() {
+        assert_eq!(288957, process_part2(input));
+    }
+
+    #[test]
+    fn part2_builds_paren() {
+        match chunk_2("(") {
+            Ok((leftover_input, v)) => {
+                assert_eq!(
+                    v,
+                    Ast {
+                        left: '(',
+                        right: None,
+                        children: vec![]
+                    }
+                );
+            }
+            Err(e) => {
+                dbg!(e);
+                assert_eq!(true, false);
+            }
+        }
+    }
+    #[test]
+    fn part2_builds_parens_and_more() {
+        match chunk_2("({") {
+            Ok((leftover_input, v)) => {
+                assert_eq!(
+                    v,
+                    Ast {
+                        left: '(',
+                        right: None,
+                        children: vec![Ast {
+                            left: '{',
+                            right: None,
+                            children: vec![]
+                        }]
+                    }
+                );
+            }
+            Err(e) => {
+                dbg!(e);
+                assert_eq!(true, false);
+            }
+        }
+    }
 }
