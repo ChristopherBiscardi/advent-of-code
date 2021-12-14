27052547071185bcdf91abbd68fd99f67ecc2652
Christopher Biscardi <chris@christopherbiscardi.com>
2021-12-14 12:21:01 -0800

    readme, day14

---
+++ 2021/rust/day-14/README.md
@@ -0,0 +1,57 @@
+# Advent of Code Day 11
+
+## Part 1
+
+dhat
+
+```
+3009
+dhat: Total:     469,425 bytes in 19,545 blocks
+dhat: At t-gmax: 152,133 bytes in 15 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data in dhat-heap.json is viewable with dhat/dh_view.html
+```
+
+criterion
+
+```
+part1                   time:   [4.1651 ms 4.1826 ms 4.2019 ms]
+```
+
+### Comparison to using HashMap instead of BTreeMap
+
+```
+3009
+dhat: Total:     468,905 bytes in 19,534 blocks
+dhat: At t-gmax: 152,277 bytes in 6 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data in dhat-heap.json is viewable with dhat/dh_view.html
+```
+
+## Part 2
+
+dhat
+
+```
+2188189693529
+dhat: Total:     27,818 bytes in 125 blocks
+dhat: At t-gmax: 2,005 bytes in 10 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data in dhat-heap.json is viewable with dhat/dh_view.html
+```
+
+criterion
+
+```
+part2                   time:   [482.08 us 485.98 us 490.06 us]
+```
+
+### Comparison to using HashMap instead of BTreeMap
+
+```
+2188189693529
+dhat: Total:     42,538 bytes in 164 blocks
+dhat: At t-gmax: 1,973 bytes in 5 blocks
+dhat: At t-end:  1,024 bytes in 1 blocks
+dhat: The data in dhat-heap.json is viewable with dhat/dh_view.html
+```
---
+++ 2021/rust/day-14/src/lib.rs
@@ -0,0 +1,160 @@
+use std::io::Write;
+use std::{collections::HashMap, fs::File};
+
+use itertools::Itertools;
+use ndarray::{s, Array2, Axis, Zip};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{
+        self, alpha1, anychar, newline, u32,
+    },
+    multi::{many1, separated_list1},
+    sequence::{pair, separated_pair},
+    IResult,
+};
+use std::fs;
+
+fn puzzle_input(
+    input: &str,
+) -> IResult<&str, (&str, HashMap<(char, char), char>)> {
+    let (input, initial_state) = alpha1(input)?;
+    let (input, _) = newline(input)?;
+    let (input, _) = newline(input)?;
+    let (input, rules) = separated_list1(
+        newline,
+        separated_pair(
+            pair(anychar, anychar),
+            tag(" -> "),
+            anychar,
+        ),
+    )(input)?;
+
+    let ruleset = rules
+        .into_iter()
+        .collect::<HashMap<(char, char), char>>();
+
+    Ok((input, (initial_state, ruleset)))
+}
+pub fn process_part1(input: &str) -> usize {
+    let (_, (initial_state, ruleset)) =
+        puzzle_input(input).unwrap();
+
+    let mut state = initial_state.to_string();
+    for _ in 0..10 {
+        // dbg!(&state);
+        let last = state.chars().last().unwrap().clone();
+        state = state
+            .chars()
+            .tuple_windows()
+            .map(|pair: (char, char)| {
+                let new_char = ruleset.get(&pair).unwrap();
+                format!("{}{}", pair.0, new_char)
+            })
+            .collect::<String>();
+        state.push(last);
+    }
+    let groups = state
+        .chars()
+        .sorted()
+        .group_by(|c| *c)
+        .into_iter()
+        .map(|(c, group)| (c, group.count()))
+        .collect::<Vec<(char, usize)>>();
+    let max = groups
+        .iter()
+        .max_by_key(|(_, count)| count)
+        .unwrap();
+    let min = groups
+        .iter()
+        .min_by_key(|(_, count)| count)
+        .unwrap();
+    max.1 - min.1
+}
+
+pub fn process_part2(input: &str) -> usize {
+    let (_, (initial_state, ruleset)) =
+        puzzle_input(input).unwrap();
+
+    let mut state: HashMap<(char, char), usize> =
+        HashMap::new();
+    for tuple in initial_state.chars().tuple_windows() {
+        state
+            .entry(tuple)
+            .and_modify(|count| {
+                *count += 1;
+            })
+            .or_insert(1);
+    }
+
+    for _ in 0..40 {
+        let mut new_state: HashMap<(char, char), usize> =
+            HashMap::new();
+        for (pair, pair_count) in state.iter() {
+            let new_char = ruleset.get(&pair).unwrap();
+
+            new_state
+                .entry((pair.0, *new_char))
+                .and_modify(|count| {
+                    *count += pair_count;
+                })
+                .or_insert(*pair_count);
+            new_state
+                .entry((*new_char, pair.1))
+                .and_modify(|count| {
+                    *count += pair_count;
+                })
+                .or_insert(*pair_count);
+        }
+        state = new_state
+    }
+
+    let mut new_counts: HashMap<char, usize> =
+        HashMap::new();
+
+    for (c, count) in
+        state.iter().map(|((a, b), count)| (a, count))
+    {
+        new_counts
+            .entry(*c)
+            .and_modify(|v| {
+                *v += count;
+            })
+            .or_insert(*count);
+    }
+    new_counts
+        .entry(initial_state.chars().last().unwrap())
+        .and_modify(|v| {
+            *v += 1;
+        })
+        .or_insert(1);
+    // for (c, thing) in new_counts.iter() {
+    //     dbg!(c, thing);
+    // }
+
+    let max = new_counts
+        .iter()
+        .max_by_key(|(_, count)| *count)
+        .unwrap();
+    let min = new_counts
+        .iter()
+        .min_by_key(|(_, count)| *count)
+        .unwrap();
+    max.1 - min.1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str =
+        include_str!("./test-input.txt");
+
+    #[test]
+    fn part1_test_demo_data() {
+        assert_eq!(1588, process_part1(INPUT));
+    }
+    #[test]
+    fn part2_test_demo_data() {
+        assert_eq!(2188189693529, process_part2(INPUT));
+    }
+}
