01b53bc082f669c1af5deeb3f900fadb6a5068bf
Christopher Biscardi <chris@christopherbiscardi.com>
2022-12-15 19:34:38 -0800

    day 15 refactor

--- 2022/rust/day-15/src/lib.rs
+++ 2022/rust/day-15/src/lib.rs
@@ -7,16 +7,58 @@ use nom::{
     sequence::{preceded, separated_pair},
     *,
 };
-use rayon::prelude::*;
 use std::{
     collections::BTreeMap,
-    ops::{RangeBounds, RangeInclusive},
+    ops::{Range, RangeInclusive},
 };
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug)]
 struct Sensor {
     x: i64,
     y: i64,
+}
+impl Sensor {
+    fn distance_to_beacon(&self, beacon: &Beacon) -> i64 {
+        (beacon.x - self.x).abs()
+            + (beacon.y - self.y).abs()
+    }
+    fn y_range(&self, max_distance: i64) -> Range<i64> {
+        (self.y - max_distance)..(self.y + max_distance)
+    }
+    /// check whether a given y-index is reachable by
+    /// the given sensor
+    fn covers(&self, distance: i64, y_index: i64) -> bool {
+        let sensor_range = self.y_range(distance);
+        sensor_range.contains(&y_index)
+    }
+    /// returns a range of the x values, centered on the
+    /// sensor's x position, that the sensor can sense
+    /// at the target y index
+    fn x_coverage_at_y(
+        &self,
+        max_distance: i64,
+        target_y_index: i64,
+    ) -> RangeInclusive<i64> {
+        let distance_to_line = self.y - target_y_index;
+
+        let max_distance_on_line =
+            max_distance - distance_to_line.abs();
+
+        (self.x - max_distance_on_line)
+            ..=self.x + max_distance_on_line
+    }
+    fn coverage_in_x_ranges(
+        &self,
+        max_distance: i64,
+    ) -> Vec<(i64, RangeInclusive<i64>)> {
+        self.y_range(max_distance)
+            .map(|y| {
+                let x_range =
+                    self.x_coverage_at_y(max_distance, y);
+                (y, x_range)
+            })
+            .collect()
+    }
 }
 #[derive(Debug, PartialEq)]
 struct Beacon {
@@ -52,41 +94,44 @@ fn map(
     ))
 }
 
+fn merge_ranges(
+    mut acc: (RangeInclusive<i64>, Option<i64>),
+    range: &RangeInclusive<i64>,
+) -> (RangeInclusive<i64>, Option<i64>) {
+    if acc.1.is_some() {
+        return acc;
+    }
+    if acc.0.end() + 1 >= *range.start() {
+        acc.0 = *acc.0.start()
+            ..=(*acc.0.end().max(range.end()));
+    } else {
+        acc.1 = Some(acc.0.end() + 1);
+    }
+
+    acc
+}
+
 pub fn process_part1(
     input: &str,
     line_number: i64,
 ) -> String {
     let (_, map) = map(input).unwrap();
-    let distances: BTreeMap<&Sensor, i64> = map
-        .iter()
-        .map(|(sensor, beacon)| {
-            (
-                sensor,
-                (beacon.x - sensor.x).abs()
-                    + (beacon.y - sensor.y).abs(),
-            )
+    map.iter()
+        .filter_map(|(sensor, closest_beacon)| {
+            let distance =
+                sensor.distance_to_beacon(closest_beacon);
+            if sensor.covers(distance, line_number) {
+                Some(
+                    sensor.x_coverage_at_y(
+                        distance,
+                        line_number,
+                    ),
+                )
+            } else {
+                None
+            }
         })
-        .collect();
-
-    distances
-        .iter()
-        .filter(|(sensor, distance)| {
-            let sensor_range = (sensor.y - **distance)
-                ..(sensor.y + **distance);
-            sensor_range.contains(&line_number)
-        })
-        .flat_map(|(sensor, max_distance)| {
-            // let width = distance * 2 + 1;
-            let distance_to_line = sensor.y - line_number;
-            // let direction_to_line =
-            //     distance_to_line.signum();
-
-            let max_distance_on_line =
-                max_distance - distance_to_line.abs();
-
-            (sensor.x - max_distance_on_line)
-                ..=sensor.x + max_distance_on_line
-        })
+        .flatten()
         .unique()
         .filter(|x| {
             !map.values().contains(&Beacon {
@@ -100,77 +145,46 @@ pub fn process_part1(
 
 pub fn process_part2(input: &str, limit: i64) -> String {
     let (_, map) = map(input).unwrap();
-    let distances: BTreeMap<&Sensor, i64> = map
-        .iter()
-        .map(|(sensor, beacon)| {
-            (
-                sensor,
-                (beacon.x - sensor.x).abs()
-                    + (beacon.y - sensor.y).abs(),
-            )
-        })
-        .collect();
-
-    let mut low_high: BTreeMap<
+    let ranges_by_y_index: BTreeMap<
         i64,
         Vec<RangeInclusive<i64>>,
-    > = BTreeMap::new();
-    for (y, range) in distances.iter().flat_map(
-        |(sensor, max_distance)| {
-            ((sensor.y - max_distance)
-                ..(sensor.y + max_distance))
-                .map(|y| {
-                    let distance_to_line = sensor.y - y;
+    > = map
+        .iter()
+        .flat_map(|(sensor, closest_beacon)| {
+            let max_distance =
+                sensor.distance_to_beacon(closest_beacon);
+            let ranges =
+                sensor.coverage_in_x_ranges(max_distance);
+            ranges.into_iter().map(|(y, range)| {
+                (
+                    y,
+                    *range.start().max(&0)
+                        ..=*range.end().min(&limit),
+                )
+            })
+        })
+        .filter(|(y, _)| y >= &0 && y <= &limit)
+        .fold(
+            BTreeMap::new(),
+            |mut acc, (y, range)| {
+                acc.entry(y)
+                    .and_modify(|ranges| {
+                        ranges.push(range.clone())
+                    })
+                    .or_insert(vec![range]);
+                acc
+            },
+        );
 
-                    let max_distance_on_line = *max_distance
-                        - distance_to_line.abs();
-
-                    (
-                        y,
-                        ((sensor.x - max_distance_on_line)
-                            .max(0))
-                            ..=((sensor.x
-                                + max_distance_on_line)
-                                .min(limit)),
-                    )
-                })
-        },
-    ) {
-        if y >= 0 && y <= limit {
-            low_high
-                .entry(y)
-                .and_modify(|lh| lh.push(range.clone()))
-                .or_insert(vec![range]);
-        }
-    }
-
-    let (x, y) = low_high
+    let (x, y) = ranges_by_y_index
         .into_iter()
-        .find_map(|(key, mut ranges)| {
-            // println!("{}", key);
+        .find_map(|(y_index, mut ranges)| {
             ranges.sort_by(|a, b| a.start().cmp(b.start()));
             let result: (RangeInclusive<i64>, Option<i64>) =
-                ranges.iter().fold(
-                    (0..=0, None),
-                    |mut acc, range| {
-                        if acc.1.is_some() {
-                            return acc;
-                        }
-                        if acc.0.end() + 1 >= *range.start()
-                        {
-                            acc.0 = *acc.0.start()
-                                ..=(*acc
-                                    .0
-                                    .end()
-                                    .max(range.end()));
-                        } else {
-                            acc.1 = Some(acc.0.end() + 1);
-                        }
-
-                        acc
-                    },
-                );
-            result.1.map(|x| (x, key))
+                ranges
+                    .iter()
+                    .fold((0..=0, None), merge_ranges);
+            result.1.map(|x| (x, y_index))
         })
         .unwrap();
 
