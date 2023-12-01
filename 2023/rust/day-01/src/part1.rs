76c5ca80795336e465c1272d99147a069162de56
Christopher Biscardi <chris@christopherbiscardi.com>
2023-12-01 01:08:27 -0800

    day 1

---
+++ 2023/rust/day-01/src/part1.rs
@@ -0,0 +1,42 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+    let output = input
+        .lines()
+        .map(|line| {
+            let mut it =
+                line.chars().filter_map(|character| {
+                    character.to_digit(10)
+                });
+            let first =
+                it.next().expect("should be a number");
+
+            match it.last() {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .expect("should be a valid number")
+        })
+        .sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
+        Ok(())
+    }
+}
---
+++ 2023/rust/day-01/src/part2.rs
@@ -0,0 +1,87 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+    let output =
+        input.lines().map(process_line).sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut it = (0..line.len()).filter_map(|index| {
+        let reduced_line = &line[index..];
+        let result = if reduced_line.starts_with("one") {
+            '1'
+        } else if reduced_line.starts_with("two") {
+            '2'
+        } else if reduced_line.starts_with("three") {
+            '3'
+        } else if reduced_line.starts_with("four") {
+            '4'
+        } else if reduced_line.starts_with("five") {
+            '5'
+        } else if reduced_line.starts_with("six") {
+            '6'
+        } else if reduced_line.starts_with("seven") {
+            '7'
+        } else if reduced_line.starts_with("eight") {
+            '8'
+        } else if reduced_line.starts_with("nine") {
+            '9'
+        } else {
+            reduced_line.chars().next().unwrap()
+        };
+
+        result.to_digit(10)
+    });
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("should be a valid number")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    /// this test case is from the real input
+    /// it tests two overlapping numbers
+    /// where the second number should succeed
+    #[case("fivezg8jmf6hrxnhgxxttwoneg", 51)]
+    fn line_test(
+        #[case] line: &str,
+        #[case] expected: u32,
+    ) {
+        assert_eq!(expected, process_line(line))
+    }
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
+        Ok(())
+    }
+}
