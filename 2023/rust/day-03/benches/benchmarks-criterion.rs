dafde982166a4edca9a24f978c6a202fc7175dd2
Christopher Biscardi <chris@christopherbiscardi.com>
2023-12-03 02:50:31 -0800

    first parser for part1

--- 2023/rust/day-03/benches/benchmarks-criterion.rs
+++ 2023/rust/day-03/benches/benchmarks-criterion.rs
@@ -10,7 +10,11 @@ fn criterion_benchmark_part1(c: &mut Criterion) {
     group.bench_with_input("part1", input, |b, input| {
         b.iter(|| part1::process(input))
     });
-
+    group.bench_with_input(
+        "part1_nom",
+        input,
+        |b, input| b.iter(|| part1_nom::process(input)),
+    );
     group.finish();
 }
 
