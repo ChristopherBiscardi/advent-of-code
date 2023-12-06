3b00d6e270a90456289763e4697ceb608f7447a1
Christopher Biscardi <chris@christopherbiscardi.com>
2023-12-06 08:56:49 -0800

    if => match with guards

--- 2023/rust/day-01/src/part2.rs
+++ 2023/rust/day-01/src/part2.rs
@@ -14,30 +14,19 @@ pub fn process(
 fn process_line(line: &str) -> u32 {
     let mut it = (0..line.len()).filter_map(|index| {
         let reduced_line = &line[index..];
-        let result = if reduced_line.starts_with("one") {
-            Some(1)
-        } else if reduced_line.starts_with("two") {
-            Some(2)
-        } else if reduced_line.starts_with("three") {
-            Some(3)
-        } else if reduced_line.starts_with("four") {
-            Some(4)
-        } else if reduced_line.starts_with("five") {
-            Some(5)
-        } else if reduced_line.starts_with("six") {
-            Some(6)
-        } else if reduced_line.starts_with("seven") {
-            Some(7)
-        } else if reduced_line.starts_with("eight") {
-            Some(8)
-        } else if reduced_line.starts_with("nine") {
-            Some(9)
-        } else {
-            reduced_line
-                .chars()
-                .next()
-                .unwrap()
-                .to_digit(10)
+        let result = match reduced_line {
+            line if line.starts_with("one") => Some(1),
+            line if line.starts_with("two") => Some(2),
+            line if line.starts_with("three") => Some(3),
+            line if line.starts_with("four") => Some(4),
+            line if line.starts_with("five") => Some(5),
+            line if line.starts_with("six") => Some(6),
+            line if line.starts_with("seven") => Some(7),
+            line if line.starts_with("eight") => Some(8),
+            line if line.starts_with("nine") => Some(9),
+            line => {
+                line.chars().next().unwrap().to_digit(10)
+            }
         };
 
         result
