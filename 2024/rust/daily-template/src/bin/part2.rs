8c254d46769d770531e3326fcfaae62e2faebb09
Christopher Biscardi <chris@christopherbiscardi.com>
2024-11-30 17:56:07 -0800

    update template

---
+++ 2024/rust/daily-template/src/bin/part2.rs
@@ -0,0 +1,12 @@
+use {{crate_name}}::part2::process;
+use miette::Context;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input2.txt");
+    let result = process(file).context("process part 2")?;
+    println!("{}", result);
+    Ok(())
+}
