88201e2fd53c8cecf23bedaf982c0c947a67da09
Christopher Biscardi <chris@christopherbiscardi.com>
2024-12-01 22:30:12 -0800

    day 2, 2024

---
+++ 2024/rust/day-02/src/bin/part2.rs
@@ -0,0 +1,12 @@
+use day_02::part2::process;
+use miette::Context;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input2.txt");
+    let result = process(file).context("process part 2")?;
+    println!("{}", result);
+    Ok(())
+}
---
+++ 2024/rust/day-02/src/part1.rs
@@ -0,0 +1,140 @@
+use itertools::Itertools;
+use miette::miette;
+use nom::{
+    character::complete::{self, newline, space1},
+    multi::separated_list1,
+    IResult,
+};
+use tracing::{info, instrument};
+
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_, reports) = parse(input)
+        .map_err(|e| miette!("parse failed {}", e))?;
+    let result = reports
+        .iter()
+        .filter(|report| check_safety(report).is_ok())
+        .count();
+    Ok(result.to_string())
+}
+
+#[instrument(ret)]
+fn check_safety(report: &Report) -> Result<(), String> {
+    let mut direction: Option<Direction> = None;
+    for (a, b) in report.iter().tuple_windows() {
+        let diff = a - b;
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Increasing) => {
+                    return Err(format!(
+                        "{}, {} switched to increasing",
+                        a, b
+                    ));
+                }
+                Some(Direction::Decreasing) => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!(
+                            "{}, {} diff value is {}",
+                            a,
+                            b,
+                            diff.abs()
+                        ));
+                    } else {
+                        continue;
+                    }
+                }
+                None => {
+                    if !(1..=3).contains(&diff.abs()) {
+                        return Err(format!(
+                            "{}, {} diff value is {}",
+                            a,
+                            b,
+                            diff.abs()
+                        ));
+                    } else {
+                        direction =
+                            Some(Direction::Decreasing);
+                        continue;
+                    }
+                }
+            },
+            1 => match direction {
+                Some(Direction::Increasing) => {
+                    if !(1..=3).contains(&diff) {
+                        return Err(format!(
+                            "{}, {} diff value is {}",
+                            a,
+                            b,
+                            diff.abs()
+                        ));
+                    } else {
+                        continue;
+                    }
+                }
+                Some(Direction::Decreasing) => {
+                    return Err(format!(
+                        "{}, {} switched to decreasing",
+                        a, b
+                    ));
+                }
+                None => {
+                    if !(1..=3).contains(&diff) {
+                        return Err(format!(
+                            "{}, {} diff value is {}",
+                            a,
+                            b,
+                            diff.abs()
+                        ));
+                    } else {
+                        direction =
+                            Some(Direction::Increasing);
+                        continue;
+                    }
+                }
+            },
+            0 => {
+                return Err(format!(
+                    "{}, {} diff was 0",
+                    a, b
+                ));
+            }
+            _ => {
+                panic!(
+                    "should never have a non -1,1,0 number"
+                );
+            }
+        }
+    }
+    Ok(())
+}
+
+type Report = Vec<i32>;
+
+fn parse(input: &str) -> IResult<&str, Vec<Report>> {
+    separated_list1(
+        newline,
+        separated_list1(space1, complete::i32),
+    )(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", process(input)?);
+        Ok(())
+    }
+}
