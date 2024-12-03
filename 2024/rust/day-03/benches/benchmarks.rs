03b8e9a477c56712be19b51d242c1f110c22a080
Christopher Biscardi <chris@christopherbiscardi.com>
2024-12-03 00:30:30 -0800

    bench

--- 2024/rust/day-03/benches/benchmarks.rs
+++ 2024/rust/day-03/benches/benchmarks.rs
@@ -22,6 +22,14 @@ fn part1_inline() {
 }
 
 #[divan::bench]
+fn part1_inline_find() {
+    part1_inline_find::process(divan::black_box(
+        include_str!("../input1.txt",),
+    ))
+    .unwrap();
+}
+
+#[divan::bench]
 fn part2() {
     part2::process(divan::black_box(include_str!(
         "../input2.txt",
