77af60225e5fda2b3c46029e8ec6c04390caec94
Christopher Biscardi <chris@christopherbiscardi.com>
2024-12-10 11:17:24 -0800

    slight part1 improvements for day10

--- 2024/rust/day-10/src/part1.rs
+++ 2024/rust/day-10/src/part1.rs
@@ -8,13 +8,11 @@ use nom::{
 use nom_locate::{position, LocatedSpan};
 use std::collections::{HashMap, HashSet};
 
-const DIRECTIONS: [IVec2; 4] =
-    [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
+const DIRECTIONS: [IVec2; 4] = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let (_, map) = parse(Span::new(input))
-        .map_err(|e| miette!("parse failed {}", e))?;
+    let (_, map) = parse(Span::new(input)).map_err(|e| miette!("parse failed {}", e))?;
 
     let counts: usize = map
         .iter()
@@ -25,50 +23,32 @@ pub fn process(input: &str) -> miette::Result<String> {
     Ok(counts.to_string())
 }
 
-fn search_trail(
-    map: &HashMap<IVec2, u32>,
-    position: &IVec2,
-) -> usize {
+fn search_trail(map: &HashMap<IVec2, u32>, position: &IVec2) -> usize {
     let mut visited: HashSet<IVec2> = HashSet::from([]);
-    let mut new_locations: HashSet<IVec2> =
-        HashSet::from([*position]);
-    loop {
-        if new_locations.is_empty() {
-            break;
-        }
+    let mut new_locations: HashSet<IVec2> = HashSet::from([*position]);
+    while !new_locations.is_empty() {
         let newer_locations = new_locations
             .iter()
             .flat_map(|starting_location| {
                 DIRECTIONS
                     .iter()
-                    .zip(std::iter::repeat(
-                        starting_location,
-                    ))
-                    .map(|(dir, location)| {
-                        (dir + location, location)
-                    })
-                    .filter(|(new_location, location)| {
-                        !visited.contains(new_location)
-                            && !new_locations
-                                .contains(new_location)
-                            && map
-                                .get(new_location)
-                                .is_some_and(|h| {
-                                    let current_height =
-                                        map.get(location)
-                                            .unwrap();
-
-                                    *h == current_height + 1
-                                })
-                    })
-                    .map(|(new, _)| new)
+                    .map(move |dir| (dir + starting_location, starting_location))
             })
+            .filter(|(new_location, starting_location)| {
+                !new_locations.contains(new_location)
+                    && map.get(new_location).is_some_and(|h| {
+                        let current_height = map.get(starting_location).unwrap();
+
+                        *h == current_height + 1
+                    })
+            })
+            .map(|(new, _)| new)
             .collect::<HashSet<IVec2>>();
 
-        visited = visited
-            .union(&newer_locations)
-            .cloned()
-            .collect::<HashSet<IVec2>>();
+        for loc in newer_locations.iter() {
+            visited.insert(*loc);
+        }
+
         new_locations = newer_locations;
     }
     visited
@@ -78,28 +58,15 @@ fn search_trail(
 }
 
 pub type Span<'a> = LocatedSpan<&'a str>;
-fn alphanum_pos(
-    input: Span,
-) -> IResult<Span, (IVec2, u32)> {
+fn alphanum_pos(input: Span) -> IResult<Span, (IVec2, u32)> {
     let (input, pos) = position(input)?;
     let x = pos.get_column() as i32 - 1;
     let y = pos.location_line() as i32 - 1;
     let (input, c) = satisfy(|c| c.is_numeric())(input)?;
-    Ok((
-        input,
-        (
-            IVec2::new(x, y),
-            c.to_digit(10).unwrap(),
-        ),
-    ))
+    Ok((input, (IVec2::new(x, y), c.to_digit(10).unwrap())))
 }
-pub fn parse(
-    input: Span,
-) -> IResult<Span, HashMap<IVec2, u32>> {
-    let (input, lines) = separated_list1(
-        line_ending,
-        many1(alphanum_pos),
-    )(input)?;
+pub fn parse(input: Span) -> IResult<Span, HashMap<IVec2, u32>> {
+    let (input, lines) = separated_list1(line_ending, many1(alphanum_pos))(input)?;
 
     let hashmap = lines
         .iter()
