22e1f84a3640787711bdb7a7f9a4bafb7633e0fc
Christopher Biscardi <chris@christopherbiscardi.com>
2024-12-15 22:33:26 -0800

    day 16

---
+++ 2024/rust/day-16/src/part2.rs
@@ -0,0 +1,160 @@
+use glam::IVec2;
+use miette::miette;
+use nom::{
+    character::complete::{line_ending, one_of},
+    combinator::{all_consuming, opt},
+    multi::{many1, separated_list1},
+    IResult,
+};
+use nom_locate::LocatedSpan;
+use pathfinding::prelude::*;
+use std::collections::HashSet;
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_input, Map { start, end, walls }) =
+        all_consuming(parse)(Span::new(input))
+            .map_err(|e| miette!("parse failed {}", e))?;
+
+    let (result, _cost) = astar_bag(
+        &(start, IVec2::X),
+        |(position, direction)| {
+            let next_pos = position + direction;
+            if walls.contains(&next_pos) {
+                vec![
+                    ((*position, direction.perp()), 1000),
+                    ((*position, -direction.perp()), 1000),
+                ]
+            } else {
+                vec![
+                    ((next_pos, *direction), 1),
+                    ((*position, direction.perp()), 1000),
+                    ((*position, -direction.perp()), 1000),
+                ]
+            }
+        },
+        |_| 0,
+        |&(pos, _)| pos == end,
+    )
+    .expect("a valid aoc result");
+
+    // dbg!(cost);
+    let set = result
+        .flat_map(|path| {
+            path.into_iter().map(|(position, _)| position)
+        })
+        .collect::<HashSet<IVec2>>();
+
+    Ok(set.len().to_string())
+    // Ok(result.1.to_string())
+}
+
+pub type Span<'a> = LocatedSpan<&'a str>;
+
+fn token(input: Span) -> IResult<Span, (IVec2, char)> {
+    let y = input.location_line();
+    let x = input.get_column();
+    let (input, token) = one_of(".#SE")(input)?;
+
+    Ok((
+        input,
+        (
+            IVec2::new(x as i32 - 1, y as i32 - 1),
+            token,
+        ),
+    ))
+}
+
+pub struct Map {
+    pub start: IVec2,
+    pub end: IVec2,
+    pub walls: HashSet<IVec2>,
+}
+pub fn parse(input: Span) -> IResult<Span, Map> {
+    let (input, items) =
+        separated_list1(line_ending, many1(token))(input)?;
+    let (input, _) = opt(line_ending)(input)?;
+
+    let (starting_position, _) = items
+        .iter()
+        .flatten()
+        .find(|(_, value)| value == &'S')
+        .cloned()
+        .expect("should have a player");
+    let (ending_position, _) = items
+        .iter()
+        .flatten()
+        .find(|(_, value)| value == &'E')
+        .cloned()
+        .expect("should have a player");
+    let walls = items
+        .into_iter()
+        .flatten()
+        .filter_map(|(pos, value)| {
+            (value == '#').then_some(pos)
+        })
+        .collect::<HashSet<IVec2>>();
+
+    Ok((
+        input,
+        Map {
+            start: starting_position,
+            end: ending_position,
+            walls: walls,
+        },
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(
+        "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############",
+        "45"
+    )]
+    #[case(
+        "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################",
+        "64"
+    )]
+    fn test_process(
+        #[case] input: &str,
+        #[case] result: &str,
+    ) -> miette::Result<()> {
+        assert_eq!(result, process(input)?);
+        Ok(())
+    }
+}
