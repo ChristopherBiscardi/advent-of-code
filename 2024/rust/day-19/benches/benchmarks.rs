fc8b6ee2f611c871d8be6c5f9542cd99c47622f6
Christopher Biscardi <chris@christopherbiscardi.com>
2024-12-19 11:29:46 -0800

    strip prefix

--- 2024/rust/day-19/benches/benchmarks.rs
+++ 2024/rust/day-19/benches/benchmarks.rs
@@ -30,6 +30,14 @@ fn part1_nom_is_ok() {
 }
 
 #[divan::bench]
+fn part1_strip_prefix() {
+    part1_strip_prefix::process(divan::black_box(
+        include_str!("../input1.txt",),
+    ))
+    .unwrap();
+}
+
+#[divan::bench]
 fn part2() {
     part2::process(divan::black_box(include_str!(
         "../input2.txt",
--- 2024/rust/day-19/src/lib.rs
+++ 2024/rust/day-19/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod part1;
 pub mod part1_nom;
 pub mod part1_nom_is_ok;
+pub mod part1_strip_prefix;
 pub mod part2;
