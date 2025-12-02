f2ed674dd26a10a36c1838478bc9e5aa9c18c0ea
Christopher Biscardi <chris@christopherbiscardi.com>
2025-12-01 22:24:10 -0800

    day 02

---
+++ 2025/rust/day-02/src/part1.rs
@@ -0,0 +1,53 @@
+use miette::miette;
+use nom::{
+    IResult, Parser, bytes::complete::tag,
+    character::complete, combinator::all_consuming,
+    multi::separated_list1, sequence::separated_pair,
+};
+use std::ops::RangeInclusive;
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_, id_ranges) =
+        all_consuming(ranges).parse(input).map_err(
+            |e| miette!("failed to parse aoc input, {e}"),
+        )?;
+    let mut total = 0;
+    for ids in id_ranges.into_iter() {
+        for id in ids.into_iter() {
+            let id_str = id.to_string();
+            let half = id_str.len() / 2;
+            if &id_str[..half] == &id_str[half..] {
+                total += id;
+            }
+        }
+    }
+    Ok(total.to_string())
+}
+
+fn ranges(
+    input: &str,
+) -> IResult<&str, Vec<RangeInclusive<u64>>> {
+    separated_list1(
+        tag(","),
+        separated_pair(
+            complete::u64,
+            tag("-"),
+            complete::u64,
+        )
+        .map(|(start, end)| start..=end),
+    )
+    .parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!("1227775554", process(input)?);
+        Ok(())
+    }
+}
