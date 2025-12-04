915f01e4d100b75038da216f4f8c44f431cc80d0
Christopher Biscardi <chris@christopherbiscardi.com>
2025-12-03 21:52:24 -0800

    day-04

---
+++ 2025/rust/day-04/src/part1.rs
@@ -0,0 +1,67 @@
+use std::collections::HashSet;
+
+use glam::IVec2;
+use tracing::info;
+
+const NEIGHBORS: [IVec2; 8] = [
+    IVec2::X,
+    IVec2::Y,
+    IVec2::NEG_X,
+    IVec2::NEG_Y,
+    IVec2::ONE,
+    IVec2::NEG_ONE,
+    IVec2::new(1, -1),
+    IVec2::new(-1, 1),
+];
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let positions = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(
+                move |(x, value)| {
+                    (value == '@').then_some(IVec2::new(
+                        x as i32, y as i32,
+                    ))
+                },
+            )
+        })
+        .collect::<HashSet<IVec2>>();
+
+    let count = positions
+        .iter()
+        .filter(|&position| {
+            NEIGHBORS
+                .iter()
+                .filter(|&offset| {
+                    positions.contains(&(position + offset))
+                })
+                .count()
+                < 4
+        })
+        .count();
+    Ok(count.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_log::test]
+    fn test_process() -> miette::Result<()> {
+        let input = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        assert_eq!("13", process(input)?);
+        Ok(())
+    }
+}
